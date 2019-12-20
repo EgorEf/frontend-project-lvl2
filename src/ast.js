46e5a1cf8d52f8b0ad3bdd3e052b257897a1a553
EgorEf <egoref66@gmail.com>
2019-12-21 01:30:56 +0500

    5 check: refactoring ast.js

--- src/ast.js
+++ src/ast.js
@@ -3,33 +3,34 @@ import _ from 'lodash';
 const typesNode = [
   {
     status: 'added',
-    getResult: (obj1, obj2) => _.identity(obj2),
-    getNode: (name, status, result) => ({ name, status, value: result }),
+    getNode: (name, status, firstValue, secondValue) => (
+      { name, status, value: _.identity(secondValue) }
+    ),
     check: (obj1, obj2, key) => (!_.has(obj1, key) && _.has(obj2, key)),
   },
   {
     status: 'edited',
-    getResult: (obj1, obj2) => ({ before: obj1, after: obj2 }),
-    getNode: (name, status, result) => ({ name, status, value: result }),
+    getNode: (name, status, firstValue, secondValue) => (
+      { name, status, value: { before: firstValue, after: secondValue } }
+    ),
     check: (obj1, obj2, key) => (_.has(obj2, key) && _.has(obj1, key) && (obj2[key] !== obj1[key]))
     && !(obj1[key] instanceof Object && obj2[key] instanceof Object),
   },
   {
     status: 'deleted',
-    getResult: (obj1) => _.identity(obj1),
-    getNode: (name, status, result) => ({ name, status, value: result }),
+    getNode: (name, status, firstValue) => ({ name, status, value: _.identity(firstValue) }),
     check: (obj1, obj2, key) => (!_.has(obj2, key)),
   },
   {
     status: 'unchanged',
-    getResult: (obj1) => _.identity(obj1),
-    getNode: (name, status, result) => ({ name, status, value: result }),
+    getNode: (name, status, firstValue) => ({ name, status, value: _.identity(firstValue) }),
     check: (obj1, obj2, key) => (obj2[key] === obj1[key]) && (_.has(obj1, key) && _.has(obj2, key)),
   },
   {
     status: 'nested',
-    getResult: (obj1, obj2, func) => func(obj1, obj2),
-    getNode: (name, status, result) => ({ name, status, children: result }),
+    getNode: (name, status, firstValue, secondValue, func) => (
+      { name, status, children: func(firstValue, secondValue) }
+    ),
     check: (obj1, obj2, key) => (obj1[key] instanceof Object && obj2[key] instanceof Object),
   },
 ];
@@ -43,9 +44,8 @@ const getUniqKeys = (obj1, obj2) => _.union(_.keys(obj1), _.keys(obj2));
 const getAst = (obj1, obj2) => {
   const uniqKeys = getUniqKeys(obj1, obj2);
   const result = uniqKeys.map((key) => {
-    const { status, getResult, getNode } = identification(obj1, obj2, key);
-    const resultForNode = getResult(obj1[key], obj2[key], getAst);
-    const node = getNode(key, status, resultForNode);
+    const { status, getNode } = identification(obj1, obj2, key);
+    const node = getNode(key, status, obj1[key], obj2[key], getAst);
     return node;
   });
   return result;
