2654980b8549093fce29307f69b838b888c2f19b
EgorEf <egoref66@gmail.com>
2020-01-06 04:12:42 +0500

    9check: refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -17,28 +17,21 @@ const pathToYml2 = path.resolve(__dirname, '__fixtures__/after.yml');
 const pathToIni1 = path.resolve(__dirname, '__fixtures__/before.ini');
 const pathToIni2 = path.resolve(__dirname, '__fixtures__/after.ini');
 
-const casesPretty = [[pathToJson1, pathToJson2, pathToResultPretty],
-  [pathToIni1, pathToIni2, pathToResultPretty], [pathToYml1, pathToYml2, pathToResultPretty]];
-
-const casesPlain = [[pathToJson1, pathToJson2, pathToResultPlain],
-  [pathToIni1, pathToIni2, pathToResultPlain], [pathToYml1, pathToYml2, pathToResultPlain]];
-
-const casesJson = [[pathToJson1, pathToJson2, pathToResultJson],
-  [pathToIni1, pathToIni2, pathToResultJson], [pathToYml1, pathToYml2, pathToResultJson]];
-
 const format1 = 'pretty';
 const format2 = 'plain';
 const format3 = 'json';
 
-test.each(casesPretty)('genDiff(%p,\n %p)',
-  (firstArg, secondArg, pathToResult) => {
-    expect(genDiff(firstArg, secondArg, format1)).toEqual(getExpectedResult(pathToResult));
-  });
-test.each(casesPlain)('genDiff(%p,\n %p)',
-  (firstArg, secondArg, pathToResult) => {
-    expect(genDiff(firstArg, secondArg, format2)).toEqual(getExpectedResult(pathToResult));
-  });
-test.each(casesJson)('genDiff(%p,\n %p)',
-  (firstArg, secondArg, pathToResult) => {
-    expect(genDiff(firstArg, secondArg, format3)).toEqual(getExpectedResult(pathToResult));
+const cases = [[format1, pathToJson1, pathToJson2, pathToResultPretty],
+  [format1, pathToIni1, pathToIni2, pathToResultPretty],
+  [format1, pathToYml1, pathToYml2, pathToResultPretty],
+  [format2, pathToJson1, pathToJson2, pathToResultPlain],
+  [format2, pathToIni1, pathToIni2, pathToResultPlain],
+  [format2, pathToYml1, pathToYml2, pathToResultPlain],
+  [format3, pathToJson1, pathToJson2, pathToResultJson],
+  [format3, pathToIni1, pathToIni2, pathToResultJson],
+  [format3, pathToYml1, pathToYml2, pathToResultJson]];
+
+test.each(cases)('format: %s\ngenDiff(%p,\n %p)',
+  (format, firstArg, secondArg, pathToResult) => {
+    expect(genDiff(firstArg, secondArg, format)).toEqual(getExpectedResult(pathToResult));
   });
