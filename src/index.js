64177639cd6327e7f31c46e1cca76d0c2cc5dc0e
EgorEf <egoref66@gmail.com>
2020-01-06 17:33:43 +0500

    9check: refactor index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import getRender from './formatters';
 const getType = (config) => path.extname(config).slice(1);
 
 export default (path1, path2, format) => {
-  const type1 = getType(path1);
-  const type2 = getType(path2);
   const data1 = fs.readFileSync(path1, 'utf8');
   const data2 = fs.readFileSync(path2, 'utf8');
+  const type1 = getType(path1);
+  const type2 = getType(path2);
   const obj1 = parse(type1, data1);
   const obj2 = parse(type2, data2);
   const ast = getAst(obj1, obj2);
